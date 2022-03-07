4736de2e2da9ca9e2a43addf8d351a7b5c02f177
lukas <lukas.weber.4.12@gmail.com>
2022-03-07 22:55:23 +0100

    add rich to requirements

--- makler_cli/inputs/house.py
+++ makler_cli/inputs/house.py
@@ -4,7 +4,7 @@ from makler_cli.inputs.room import input_multiple_rooms
 
 
 def input_house() -> House:
-    print('input house:')
+    print(' input house '.center(30, '-'))
     room_count = input_integer(message='how many rooms does the house have? ')
     house_rooms = input_multiple_rooms(count=room_count)
     return House(rooms=house_rooms)
