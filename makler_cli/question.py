52c40e68be155631880279be5ac5df23483d3dc2
lukas <lukas.weber.4.12@gmail.com>
2022-03-07 20:11:05 +0100

    add question to cli

---
+++ makler_cli/question.py
@@ -0,0 +1,11 @@
+def yes_no_question(message: str) -> bool:
+    while True:
+        boolean_input = input(message)
+
+        if boolean_input in ('y', 'yes'):
+            return True
+
+        elif boolean_input in ('n', 'no'):
+            return False
+
+        print('please answer with \'yes\' or \'no\'!')
