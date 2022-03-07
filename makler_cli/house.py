8f654e91bc50f91bf9f5cdce3103532b97685d4a
lukas <lukas.weber.4.12@gmail.com>
2022-03-07 22:07:35 +0100

    update house inputs

--- makler_cli/house.py
+++ makler_cli/house.py
@@ -1,9 +1,9 @@
-from makler_cli.number import input_integer
-from makler_cli.room import input_room
 from makler.house import House
+from makler_cli.number import input_integer
+from makler_cli.room import input_multiple_rooms
 
 
 def input_house() -> House:
     room_count = input_integer(message='how many rooms does the house have? ')
-    house_rooms = tuple(input_room() for _ in range(room_count))
+    house_rooms = input_multiple_rooms(count=room_count)
     return House(rooms=house_rooms)
