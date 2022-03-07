fa2d1efb8f3e2f08378025801e37f3e4443a2355
lukas <lukas.weber.4.12@gmail.com>
2022-03-07 23:10:37 +0100

    update headlines

--- makler_cli/prints.py
+++ makler_cli/prints.py
@@ -1,9 +1,17 @@
 def headline1(title: str) -> None:
-    headline(title, length=30)
+    headline(title, length=40)
 
 
 def headline2(title: str) -> None:
-    headline(title, length=24)
+    headline(title, length=34)
+
+
+def headline3(title: str) -> None:
+    headline(title, length=28)
+
+
+def headline4(title: str) -> None:
+    headline(title, length=22)
 
 
 def headline(title: str, length: int) -> None:
