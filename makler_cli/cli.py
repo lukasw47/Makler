f916667ba5052fbaa7f08e3a2969dab7bfbd52ec
lukas <lukas.weber.4.12@gmail.com>
2022-03-08 12:14:25 +0100

    refactor code

--- makler_cli/cli.py
+++ makler_cli/cli.py
@@ -7,6 +7,7 @@ def run_cli() -> None:
     try:
         while True:
             measure_house()
+            ask_to_repeat()
 
     except KeyboardInterrupt:
         pass
@@ -15,6 +16,9 @@ def run_cli() -> None:
 def measure_house() -> None:
     house = input_house()
     print_house(house)
+
+
+def ask_to_repeat() -> None:
     if yes_no_question(message='do you want to input another house [y/n]? '):
         raise KeyboardInterrupt
     print()
