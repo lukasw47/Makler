4d94d1f9a96c309dd02dffff67c1e13c1fe96a08
lukas <lukas.weber.4.12@gmail.com>
2022-03-08 12:12:43 +0100

    refactor code

--- makler_cli/inputs/house.py
+++ makler_cli/inputs/house.py
@@ -1,4 +1,7 @@
+from typing import Sequence
+
 from makler.house import House
+from makler.room import Room
 from makler_cli.inputs.number import input_integer
 from makler_cli.inputs.room import input_multiple_rooms
 from makler_cli.prints import headline1, headline2
@@ -6,7 +9,11 @@ from makler_cli.prints import headline1, headline2
 
 def input_house() -> House:
     headline1(title='house')
+    house_rooms = input_house_rooms()
+    return House(rooms=house_rooms)
+
+
+def input_house_rooms() -> Sequence[Room]:
     room_count = input_integer(message='how many rooms does the house have? ')
     print()
-    house_rooms = input_multiple_rooms(count=room_count)
-    return House(rooms=house_rooms)
+    return input_multiple_rooms(count=room_count)
