c57ff23c43f8b1b97e69e89e0f6aebb510d5a2a0
lukas <lukas.weber.4.12@gmail.com>
2022-03-08 12:15:42 +0100

    reformt code

--- makler_cli/inputs/number.py
+++ makler_cli/inputs/number.py
@@ -4,12 +4,14 @@ from decimal import Decimal
 def input_integer(message: str) -> int:
     while not (integer_value := input(message)) or not integer_value.isdecimal():
         print('please input a whole number!')
+
     return int(integer_value)
 
 
 def input_decimal(message: str) -> Decimal:
     while not (decimal_text := input(message)) or not is_decimal_number(decimal_text):
         print('please input a number!')
+
     decimal_value = format_decimal_text(decimal_text)
     return Decimal(decimal_value)
 
