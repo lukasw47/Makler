599f80bfe2165e427dd937d3e4889d40fceb7f2c
lukas <lukas.weber.4.12@gmail.com>
2022-03-16 23:22:20 +0100

    make code more readable

--- makler_cli/inputs/number.py
+++ makler_cli/inputs/number.py
@@ -2,14 +2,16 @@ from decimal import Decimal
 
 
 def input_integer(message: str) -> int:
-    while not (integer_value := input(message)) or not integer_value.isdecimal():
+    while (integer_value := input(message)) == '' \
+            or not integer_value.isdigit():
         print('please input a whole number!')
 
     return int(integer_value)
 
 
 def input_decimal(message: str) -> Decimal:
-    while not (decimal_text := input(message)) or not is_decimal_number(decimal_text):
+    while (decimal_text := input(message)) == '' \
+            or not is_decimal_number(decimal_text):
         print('please input a number!')
 
     decimal_value = format_decimal_text(decimal_text)
